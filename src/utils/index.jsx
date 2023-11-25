4f74888774c344f987e95e9c188a2e742d0fb8d2
Helena Martins <lenatinsjs@gmail.com>
2023-11-25 17:01:41 -0300

    adjustments

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,12 +1,16 @@
 export const texts = {
   PT: {
+    title: "Lista de tarefas",
     input: "Adicione uma tarefa",
-    button: "Adicione",
+    button: "Adicionar",
     toggleComplete: "Esconder tarefas concluídas",
+    errorInput: "The field shoud not be empty",
   },
   EN: {
+    title: "To do list",
     input: "Enter a task",
     button: "Add",
     toggleComplete: "Hide complete tasks",
+    errorInput: "O campo não pode estar vazio",
   },
 };
